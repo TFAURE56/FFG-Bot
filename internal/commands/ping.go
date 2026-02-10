0168930f4fd1b06cbcada52f5910308c167d9fef
TFAURE <thomas.fe2005@outlook.com>
2026-02-10 19:59:17 +0000

    update to 0.4.1

--- internal/commands/ping.go
+++ internal/commands/ping.go
@@ -19,7 +19,7 @@ func init() {
 // Handler pour la commande ping visible uniquement par le lanceur de la commande
 func pingHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	latency := s.HeartbeatLatency().Milliseconds()
-	response := fmt.Sprintf("Pong! Latence: %dms\n Je suis a ma version 0.3.0", latency)
+	response := fmt.Sprintf("Pong! Latence: %dms\n Je suis a ma version 0.4.1", latency)
 
 	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
